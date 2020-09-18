a8f220c87c7dd1165a6da6be11c6c9daecae63ac
gmmosnalve <gmmonsalve@uninorte.edu.co>
2020-09-18 16:10:40 -0500

    Quiz and Question

---
+++ Assets/Question.cs
@@ -0,0 +1,12 @@
+﻿
+public class Question {
+	
+	public string fact;
+	public bool isTrue;
+
+	public Question(string question, bool value)
+  {
+    fact = question;
+	isTrue = value;
+  }
+}
---
+++ Assets/Quiz.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class Quiz : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public ArrayList preguntas = new ArrayList();
+   
+   
+    void Start()
+    {
+        
+         preguntas.Add("Hola");
+         preguntas.Add("soy");
+         preguntas.Add("una prueba");
+         Debug.Log("Sí estoy aquí jaja");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setQuestions(){
+    }
+
+    public string getQuestion(){
+        
+      string question = preguntas[0];
+      return question;
+   }
+}
