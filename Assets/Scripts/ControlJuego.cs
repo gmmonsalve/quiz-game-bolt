01eeb82e363420a974dc493321d4ec3349e4b81e
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-09-22 13:57:18 -0500

    Trabajando en el plugin

---
+++ Assets/Scripts/ControlJuego.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ludiq;
+using UnityAndroidBluetooth;
+
+public class ControlJuego : MonoBehaviour
+{
+    BluetoothControl control;
+    bool left = false;
+    bool right = false;
+
+
+    void Start()
+    {
+        control = BluetoothControl.Instance;    
+    }
+
+    public bool botonPresionado(string name){
+        return control.GetButton(name).IsPressed;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
